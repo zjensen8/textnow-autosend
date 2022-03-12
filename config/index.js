9f2a27c2ff3142f7bdf0389682670e9df1d31db4
zjensen8 <84636122+zjensen8@users.noreply.github.com>
2022-03-12 21:32:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   username: process.env.TEXTNOW_USERNAME || "",
   password: process.env.TEXTNOW_PASSWORD || "",
-  recipient: process.env.TEXTNOW_RECIPIENT || "(726) 666-0002",
-  message: process.env.TEXTNOW_MESSAGE || "autosend message",
+  recipient: process.env.TEXTNOW_RECIPIENT || "(628) 269-5288",
+  message: process.env.TEXTNOW_MESSAGE || "This's the autotext from TN to GV!",
   cookies_str: process.env.TEXTNOW_COOKIES || ""
 };
